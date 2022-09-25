5b68a7ff3f5721df2555941710c173554810132f
Tulsi <tulsiii1104@gmail.com>
2022-09-25 20:59:51 +0530

    done

--- app/src/main/java/com/example/madpractical_5_20012021041/MainActivity.kt
+++ app/src/main/java/com/example/madpractical_5_20012021041/MainActivity.kt
@@ -3,29 +3,28 @@ package com.example.madpractical_5_20012021041
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.madpractical_5_20012021041.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-    }
-
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener{
+            play()
+        })
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener{
+            stop()
+        }) }
     private fun play(){
-        Intent(applicationContext,MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_KEY).apply {
-            startService(this)
-        }
+        Intent(applicationContext, MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_VALUE).apply { startService(this) } }
+    private fun stop(){
+        Intent(applicationContext,MediaService::class.java).apply { stopService(this) }
     }
-
-   private fun stop()
-   {
-       Intent(applicationContext,MediaService::class.java).apply {
-           stopService(this)
-       }
-   }
 }
